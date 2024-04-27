326b3df6531b7f6207c4bb9b5532f46e2cc510d1
rarenatoe <renatoealegre@gmail.com>
2024-04-27 15:33:30 -0500

    fix: last line with more/less button was not updating when children changed

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import { MoreOrLess } from '@rntext/more-or-less';
-import { Platform, StyleSheet, View } from 'react-native';
+import { Button, Platform, StyleSheet, View } from 'react-native';
 import CustomText from './CustomText';
 
+const text1 = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem.`;
+const text2 =
+  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.";
 export default function App() {
+  const [flag, setFlag] = React.useState(true);
+
   return (
     <View style={styles.appContainer}>
+      <Button title="Change" onPress={() => setFlag((prev) => !prev)} />
       <View style={styles.container} />
       <View style={[styles.container, styles.center]}>
         <MoreOrLess
@@ -17,13 +23,7 @@ export default function App() {
           textButtonStyle={styles.textButton}
           animated
         >
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem.
+          {flag ? text1 : text2}
         </MoreOrLess>
       </View>
       <View style={styles.container} />
